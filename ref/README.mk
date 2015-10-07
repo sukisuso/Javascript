1fe4fc5bc3417424bbfe392d35a098127d1d7683
Jesus <jesusjuanaguilar@gmail.com>
2015-10-07 19:49:31 +0200

    LAST VERSION REF

--- ref/README.mk
+++ ref/README.mk
@@ -1,5 +1,5 @@
-# REF LIB
-* Jesús Juan Aguilar (jesusjuanaguilar@gmail.com)
+REF LIB
+Jesús Juan Aguilar (jesusjuanaguilar@gmail.com)
 
 Say goodbye to Control + ALT + F5 just include ref.js in your web see like the code is reload when your resources change.
 
