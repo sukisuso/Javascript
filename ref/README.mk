3d86bce7dd4c9fd3bec1846b09677cb3d615a2c9
Jesus <jesusjuanaguilar@gmail.com>
2015-10-07 19:52:24 +0200

    LAST VERSION REF LIB

--- ref/README.mk
+++ ref/README.mk
@@ -1,7 +1,7 @@
 REF LIB
 Jesús Juan Aguilar (jesusjuanaguilar@gmail.com)
 
-Say goodbye to Control + ALT + F5 just include ref.js in your web see like the code is reload when your resources change.
+Say goodbye to Control + ALT + F5 just include ref.js in your web and see like the code is reload when your resources change.
 
 Enjoy.
 10 - 2015.
