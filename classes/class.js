dd18027f345c84f3401bb6c9c05fcc1857a7c2a5
Jesus <jesusjuanaguilar@gmail.com>
2015-10-13 20:39:23 +0200

    Execute simple functions.

--- classes/class.js
+++ classes/class.js
@@ -12,13 +12,12 @@ Class.new = function (classtype){
 
 	var newclass = load_file_from_class_folder(Class["folder"]+classtype+Class["file_extension"]);
 	if(newclass != null){
-		return newclass;
+		return set_function_usable(newclass);
 	}
 
 	console.log("Error Class File");
 	return null;
 }
-
 
 function load_file_from_class_folder (url){
 
@@ -36,4 +35,20 @@ function load_file_from_class_folder (url){
 			return null;
 		}
     }
+}
+
+function set_function_usable (object){
+
+	for(properties in object){
+		if(properties[0] == "f" && properties[1] == ":"){
+			var function_name = properties.substring(2,properties.length);
+
+			var ff = null;
+			eval("var ff = function "+function_name+' (){var name_array = "f:"+arguments.callee.name;for(actions in this[name_array]){eval(this[name_array][actions]);}};');
+			object[function_name] = ff;
+
+		}
+	}
+
+	return object;
 }
