735a0fe1f29c8dba0a5fdf506e997f4551a112f5
Jesus <jesusjuanaguilar@gmail.com>
2015-12-06 22:14:03 +0100

    06/12/2015

--- node/mongoose/app.js
+++ node/mongoose/app.js
@@ -13,8 +13,8 @@ db.on('error', function (err) {console.log('Connection error', err);});
 db.once('open', function () {console.log('Conexion stabilised');
 
 	db.collection("person").findOne({"name":"Oscar"}, function (err, docs) {
-	  // docs is an array
-	 	//db.collection("person").remove(docs)
+	    // docs is an array
+        //db.collection("person").remove(docs)
 	 	db.collection("person").update(docs, {"name":"Jesus"});
 
 	});
@@ -24,6 +24,8 @@ var user = {
 	_id: new ObjectID(),
 	"name": "Laura"
 };
+
+
 
 //db.collection("person").insert(user);
 
