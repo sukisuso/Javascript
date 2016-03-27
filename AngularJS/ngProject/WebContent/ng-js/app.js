d7bf6c9949702e46ca357324c906553184c98cec
Jesus <jesusjuanaguilar@gmail.com>
2016-03-27 18:49:40 +0200

    27/03/2016

--- AngularJS/ngProject/WebContent/ng-js/app.js
+++ AngularJS/ngProject/WebContent/ng-js/app.js
@@ -1,9 +1,45 @@
 /**
- *  
+ *  Jesús Juan Aguilar 
  */
 
 var app = angular.module('tienda', []);
 
 app.controller('loadText', function ($scope){
 	$scope.TodosDatos = 4;
+});
+
+app.controller('studentController', function ($scope,$http) {
+    var url = "bo/data.json";
+ 
+    $http.get(url).success( function(response) {
+       $scope.students = response;
+    });
+});
+
+//Router Example
+var mainApp = angular.module("mainApp", ['ngRoute']);
+mainApp.config(['$routeProvider', function($routeProvider) {
+   $routeProvider
+   
+   .when('/addStudent', {
+      templateUrl: 'addStudent.htm',
+      controller: 'AddStudentController'
+   })
+   
+   .when('/viewStudents', {
+      templateUrl: 'viewStudents.htm',
+      controller: 'ViewStudentsController'
+   })
+   
+   .otherwise({
+      redirectTo: '/addStudent'
+   });
+}]);
+
+mainApp.controller('AddStudentController', function($scope) {
+   $scope.message = "This page will be used to display add student form";
+});
+
+mainApp.controller('ViewStudentsController', function($scope) {
+   $scope.message = "This page will be used to display all the students";
 });
